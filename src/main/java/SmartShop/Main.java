2431ed0d8d423633b578c2d0591afd4c5f2bdcb2
Supakit Theanthunyakit <mr.pokiloli@gmail.com>
2019-09-03 19:50:12 +0700

    Return all Movies in JSON

--- src/main/java/SmartShop/Main.java
+++ src/main/java/SmartShop/Main.java
@@ -1,22 +1,38 @@
 package SmartShop;
 
+import com.google.gson.Gson;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.sql.SQLOutput;
+
 
 @SpringBootApplication
 @RestController
 @EnableAutoConfiguration
 public class Main {
+    private MovieFactory movieFactory = new MovieFactory();
+    private MovieBean movie1 = movieFactory.newMovie("ATM เออรักเออเร่อ",5,7);
+    private MovieBean movie2 = movieFactory.newMovie("ชัตเตอร์ กดติดวิญญาณ", 10,7);
+    private MovieBean movie3 = movieFactory.newMovie("เพื่อนสนิท",5,7);
+    private MovieBean movie4 = movieFactory.newMovie("รถไฟฟ้ามาหานะเธอ", 10,7);
+    private MovieBean movie5 = movieFactory.newMovie("ไอฟาย..แต๊งกิ้วเลิฟยู้",5,7);
+    private MovieBean movie6 = movieFactory.newMovie("คิดถึงวิทยา", 10,7);
+    private MovieBean movie7 = movieFactory.newMovie("พรจากฟ้า",5,7);
+    private MovieBean movie8 = movieFactory.newMovie("แฟนเดย์ แฟนกันแค่วันเดียว", 10,7);
+    private MovieBean movie9 = movieFactory.newMovie("น้อง พี่ ที่รัก",5,7);
+    private MovieBean movie10 = movieFactory.newMovie("Friendzone ระวัง..สิ้นสุดทางเพื่อน", 10,7);
+
+
     public static void  main(String[] args){
         SpringApplication.run(Main.class, args);
     }
 
     @RequestMapping("/")
     String home(){
-        return "Hey This is \'BlockButter\' Video Rental Store";
+        return movieFactory.getAllMovie();
     }
 }
--- src/main/java/SmartShop/MovieFactory.java
+++ src/main/java/SmartShop/MovieFactory.java
@@ -1,14 +1,27 @@
 package SmartShop;
 
+import com.google.gson.Gson;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class MovieFactory {
     private int amountOfMovie = 0;
+    List<MovieBean> movieDB = new ArrayList<MovieBean>();
     public MovieBean newMovie(String movieName, int amountInStock, int dayToRent){
         MovieBean movie = new MovieBean(this.amountOfMovie, movieName, amountInStock, dayToRent);
         increaseAmountofMovie();
+        movieDB.add(movie);
         return movie;
     }
 
     private void increaseAmountofMovie(){
         this.amountOfMovie++;
+    }
+    
+    public String getAllMovie(){
+        Gson gson = new Gson();
+        String json = gson.toJson(movieDB);
+        return json;
     }
 }
